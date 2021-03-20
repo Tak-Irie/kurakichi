61a2a5e9dce470f1b8d19c980e8f14c8fce28bbd
Tak-Irie <tak.mibp@gmail.com>
2021-03-20 13:28:55 +0900

    clean packages

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-  preset: 'ts-jest',
-  moduleFileExtensions: ['ts', 'tsx', 'js'],
-  testEnvironment: 'node',
-  projects: [
-    '<rootDir>/apps/next-web',
-    '<rootDir>/apps/express-server',
-    '<rootDir>/libs/ddd-modules',
-  ],
+  projects: ['<rootDir>/apps/express'],
 };
