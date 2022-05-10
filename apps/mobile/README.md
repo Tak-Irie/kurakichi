05f3f055cdf1e3994ff5d4583a1e09bfa468e667
Tak-Irie <tak.mibp@gmail.com>
2022-05-10 10:13:07 +0900

    wip

--- apps/mobile/README.md
+++ apps/mobile/README.md
@@ -1,3 +1,4 @@
 # kurakichi_mobile
 
 WIP
+Kurakichi mobile will be developed by Flutter.
--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 
 import { ApolloProvider } from '@apollo/client';
-import '../../globals.css';
+import '../../styles/globals.css';
 
 import { Layout } from '../components/presentational/templates';
 import { apolloClient } from '../lib/createApolloClient';
